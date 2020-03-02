fb14219f772178c3ade98cd8de7293b40c70a5e1
Igor Laptev <igorlaptev@MacBook-Pro-Igor.local>
2020-03-02 14:38:30 +0500

    added gulp-task for conversion style.cs

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const autoprefix = require('gulp-autoprefixer');
+const minifyCSS = require('gulp-minify-css');
+const concat = require('gulp-concat');
 
 gulp.task('hello', (done)=> {
     console.log('Привет, мир!');
@@ -15,3 +18,13 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+/* минимизируетм CSS-файлы и добавляем к ним конечный .min.css */
+gulp.task('styles', function(done) {
+    gulp.src(['./*.css'])
+    .pipe(concat('style.min.css'))             /* объединяем CSS-файлы и переименовываем их */
+    .pipe(autoprefix('last 2 versions'))       /* указываем текущую и предыдущие версии всех браузеров */
+    .pipe(minifyCSS())                         /* минимизируем все CSS-скрипты из папки => копируем в папку сборки */
+    .pipe(gulp.dest('css/'));                  /* вызываем метод "dest" с аргументом, который представляет целевой каталог */
+    done();
+  });
