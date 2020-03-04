dc78516095fbe68137af8a425b91fa325ddff1c7
Igor Laptev <igorlaptev@MacBook-Pro-Igor.local>
2020-03-04 13:52:32 +0500

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,40 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const autoprefix = require('gulp-autoprefixer');
 const minifyCSS = require('gulp-minify-css');
 const concat = require('gulp-concat');
-
-gulp.task('hello', (done)=> {
-    console.log('Привет, мир!');
-    done();
-});
+const sass = require('gulp-sass');
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+    serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
 
-/* минимизируем CSS-файлы и добавляем к ним конечный .min.css */
-gulp.task('styles', function(done) {
-    gulp.src(['./*.css'])
-    .pipe(concat('style.min.css'))             /* объединяем CSS-файлы и переименовываем их */
-    .pipe(autoprefix('last 2 versions'))       /* указываем текущую и предыдущие версии всех браузеров */
-    .pipe(minifyCSS())                         /* минимизируем все CSS-скрипты из папки => копируем в папку сборки */
-    .pipe(gulp.dest('css/'));                  /* вызываем метод "dest" с аргументом, который представляет целевой каталог */
-    done();
-  });
+};
+
+// /* минимизируем CSS-файлы и добавляем к ним конечный .min.css */
+// gulp.task('styles', function(done) {
+//     gulp.src(['./*.css'])
+//     .pipe(concat('style.min.css'))             /* объединяем CSS-файлы и переименовываем их */
+//     .pipe(autoprefix('last 2 versions'))       /* указываем текущую и предыдущие версии всех браузеров */
+//     .pipe(minifyCSS())                         /* минимизируем все CSS-скрипты из папки => копируем в папку сборки */
+//     .pipe(gulp.dest('css/'));                  /* вызываем метод "dest" с аргументом, который представляет целевой каталог */
+//     done();
+//   });
+
+  // Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
