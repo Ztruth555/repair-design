8e97cd53950972d925c7f6b6ee8b89fad14acc93
Igor Laptev <igorlaptev@MacBook-Pro-Igor.local>
2020-03-06 16:22:49 +0500

    feat: add before and after elements in hero section

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-const autoprefix = require('gulp-autoprefixer');
-const minifyCSS = require('gulp-minify-css');
-const concat = require('gulp-concat');
 const sass = require('gulp-sass');
 
 // Static server
@@ -20,17 +17,7 @@ function bs() {
 
 };
 
-// /* минимизируем CSS-файлы и добавляем к ним конечный .min.css */
-// gulp.task('styles', function(done) {
-//     gulp.src(['./*.css'])
-//     .pipe(concat('style.min.css'))             /* объединяем CSS-файлы и переименовываем их */
-//     .pipe(autoprefix('last 2 versions'))       /* указываем текущую и предыдущие версии всех браузеров */
-//     .pipe(minifyCSS())                         /* минимизируем все CSS-скрипты из папки => копируем в папку сборки */
-//     .pipe(gulp.dest('css/'));                  /* вызываем метод "dest" с аргументом, который представляет целевой каталог */
-//     done();
-//   });
 
-  // Compile sass into CSS & auto-inject into browsers
 function serveSass() {
     return src("./sass/**/*.sass", "./sass/**/*.scss")
         .pipe(sass())
