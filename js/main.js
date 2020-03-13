3d3f35f97d13c8ab609677991073b3b7ef43b2b4
Igor Laptev <igorlaptev@MacBook-Pro-Igor.local>
2020-03-13 14:20:48 +0500

    feat: add up button to scroll slowly & rewrote code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) {
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -12,4 +13,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     closeBtn.addEventListener('click', switchModal);
 
-});
+}); 
+*/
+
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closelBtn = $('.modal__close'),
+        modalHidden = $('.modal__dialog');
+  
+    modalBtn.on('click', function () {
+      modal.toggleClass('modal--visible');
+    });
+    closelBtn.on('click', function () {
+      modal.toggleClass('modal--visible');
+    });
+    
+    jQuery(function($){
+      modal.mouseup(function (e){ 
+          if (!modalHidden.is(e.target) && modalHidden.has(e.target).length === 0) { 
+            modal.toggleClass('modal--visible'); 
+          }
+      });
+    });
+    
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+          if ($('#upbutton').is(':hidden')) {
+              $('#upbutton').css({opacity : 1}).fadeIn('slow');
+          }
+      } else { $('#upbutton').stop(true, false).fadeOut('fast'); }
+    });
+    $('#upbutton').click(function() {
+        $('html, body').stop().animate({scrollTop : 0}, 800);
+    });
+    
+  });
