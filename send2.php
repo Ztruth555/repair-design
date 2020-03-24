b5e98291aecbc4ac350638b002b0ce61779ae5f6
Igor Laptev <igorlaptev@MacBook-Pro-Igor.local>
2020-03-24 14:09:44 +0500

    fix: some bugs

--- send2.php
+++ send2.php
@@ -1,8 +1,8 @@
 <?php
 
 $userName = $_POST['userName'];
-$userEmail = $_POST['userEmail'];
 $userPhone = $_POST['userPhone'];
+$userQuestion = $_POST['userQuestion'];
 
 // Load Composer's autoloader
 require 'phpmailer/PHPMailer.php';
@@ -21,7 +21,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'vasya555vas@gmail.com';                     // SMTP username
-    $mail->Password   = '788olaola887';                               // SMTP password
+    $mail->Password   = '7788olaola8877';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
@@ -32,7 +32,7 @@ try {
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Новая заявка с сайта';
-    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}";
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его вопрос: ${userQuestion}";
 
     $mail->send();
     header('Location: thanks.html');
